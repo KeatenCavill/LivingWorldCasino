1cf18eff78d22f8cd67f3fe9e226f98ebf504bad
maddie-moffett <mnrmoffett@gmail.com>
2025-12-05 10:47:01 -0700

    actually do what I said the last commit did bc the last commit did nothing.

--- src/core/NPC.java
+++ src/core/NPC.java
@@ -1,11 +1,34 @@
 package core;
+import core.Item;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+// give: inventory, drunk level
+// options: fight, steal from, give, talk to
 
 public class NPC extends Person {
 
-    //NPC(){
-        // choose random name from names.txt
+    private String name;
+    private Item[] inventory;
+    private double drunkness;
+    private double anger;
+
+    public NPC(){
+
+        try {
+            List<String> lines = Files.readAllLines(Path.of("names.txt"));
+            this.name = lines.get(new Random().nextInt(lines.size()));
+        } 
+        catch(Exception err) {err.printStackTrace();}
         // choose random meter levels
-    //}
+        
+    }
     
     public void give(Item item){
 
