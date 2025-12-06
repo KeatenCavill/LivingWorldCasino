c4c1095c3f4ee8d99426842b30ffaf45bca09ebe
maddie-moffett <mnrmoffett@gmail.com>
2025-12-06 00:25:04 -0700

    copy and paste bartender into cook but make appropriate changes for now

--- src/npcs/Cook.java
+++ src/npcs/Cook.java
@@ -1,16 +1,43 @@
 package npcs;
+import java.util.List;
+
 import core.Item;
 import core.NPC;
 import core.Player;
+import items.*;
 
 
 
 public class Cook extends NPC implements Vendor {
+
+    private List<Item> forSale;
+    
+    private void populateGoods(){
+        Burger burger = new Burger(); this.forSale.add(burger);
+        Fries fries = new Fries(); this.forSale.add(fries);
+    };
+
+    public Cook(){
+        populateGoods();
+    }
     
     public void displayGoods(){
-
+        System.out.println(super.name + ": \"What do you want to eat tonight?\"");
+        /*
+        int inc = 1;
+        for(Item item : this.forSale){
+            String message = inc + ") " + item.getName() + " : $" + item.getMonetaryValue();
+            System.out.println(message);
+            inc ++;
+        }
+        */
     }
     public void buyItem(int item, Player player){
+        int itemnum = item - 1;
+        Item purchase = forSale.get(itemnum);
+        player.inventoryAdd(purchase);
+        System.out.println(super.name + ": \"Order up!\"");
 
     }
+    
 }
