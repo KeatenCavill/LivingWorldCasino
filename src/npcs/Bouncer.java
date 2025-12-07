a7da87a60e2573d1d98e472959610a195b5920bf
maddie-moffett <mnrmoffett@gmail.com>
2025-12-06 19:16:58 -0700

    add kick and bribe functions to bouncer

--- src/npcs/Bouncer.java
+++ src/npcs/Bouncer.java
@@ -4,6 +4,9 @@ import core.Player;
 
 
 public class Bouncer extends NPC {
+
+    boolean kicked = false;
+    boolean bribed = false;
     
     public Bouncer(){
         super.drunkMeter = 0;
@@ -12,11 +15,29 @@ public class Bouncer extends NPC {
     public boolean checkID(Player player){
         if(player.age < 21){
             System.out.println(super.name + ": \"What are you doing here, kid? Get out of here!\"");
+            this.kicked = true;
             return(false);
         } else{
             System.out.println(super.name + ": \"Alright, you're good to go.\"");
             return(true);
         }
+    }
+
+    public void kickOut(){
+        if(!bribed){
+            this.kicked = true;
+            System.out.println(super.name + " grabs you by the back of the shirt and hauls you outside!");
+            System.out.println(super.name + ": \"And stay out!\"");
+            // NEED A WAY TO ACTUALLY MOVE THE PLAYER'S POSITION
+        } else {
+            System.out.println(super.name + ": \"I'll let you off this time, but this is your last warning!\"");
+            this.bribed = false;
+        }
+    }
+
+    public void bribe(){
+        this.kicked = false;
+        System.out.println(super.name + ": \"You're good. Don't mention this, ya hear?\"");
     }
 
 }
