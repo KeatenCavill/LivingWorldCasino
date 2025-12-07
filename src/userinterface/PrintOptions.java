d3222b94d9192ca93f22f53c08c0203eb0ed531c
Drew Saltzman <drewsaltzman16@gmail.com>
2025-12-07 11:56:07 -0700

    Added some basic logic and userinterface files

---
+++ src/userinterface/PrintOptions.java
@@ -0,0 +1,21 @@
+package userinterface;
+
+
+public class PrintOptions {
+
+    static String[] currentEnv(core.Environment env){
+        numOptions = env.connnectedAreas.size();
+        String[] options = new String[] {""};
+        return options;
+    }
+
+    static String[] currentItem(core.Item item){
+
+    }
+
+    static String[] currentNPC(core.NPC npc){
+
+    }
+
+
+}
---
+++ src/userinterface/UserIO.java
@@ -0,0 +1,16 @@
+package userinterface;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserIO {
+    
+    static void input(Scanner inStream){
+        try {
+            
+        } 
+        catch (InputMismatchException e) {
+
+        }
+    }
+
+}
