e6ddb7d8b0ff6bed0548e0200279731d30156b51
maddie-moffett <mnrmoffett@gmail.com>
2025-12-06 17:52:34 -0700

    add checkID function to bouncer

--- src/npcs/Bouncer.java
+++ src/npcs/Bouncer.java
@@ -1,5 +1,6 @@
 package npcs;
 import core.NPC;
+import core.Player;
 
 
 public class Bouncer extends NPC {
@@ -7,4 +8,15 @@ public class Bouncer extends NPC {
     public Bouncer(){
         super.drunkMeter = 0;
     }
+    
+    public boolean checkID(Player player){
+        if(player.age < 21){
+            System.out.println(super.name + ": \"What are you doing here, kid? Get out of here!\"");
+            return(false);
+        } else{
+            System.out.println(super.name + ": \"Alright, you're good to go.\"");
+            return(true);
+        }
+    }
+
 }
