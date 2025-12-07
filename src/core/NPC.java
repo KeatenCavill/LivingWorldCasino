14be17db3837ffa9f847147e26ee7a30da0d87ac
maddie-moffett <mnrmoffett@gmail.com>
2025-12-06 19:27:23 -0700

    add the kickout to the fight function in npc; also add relieveoneself (TODO: implement that function in the person class)

--- src/core/NPC.java
+++ src/core/NPC.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Random;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import npcs.Bouncer;
 
 public class NPC extends Person {
 
@@ -44,10 +45,11 @@ public class NPC extends Person {
         System.out.println(this.phrases.get(messagenum));
     }
 
-    public void fight(Player player){
+    public void fight(Player player, Bouncer bouncer){
         if(super.peeMeter >= 0.75){
             System.out.println(super.name + ": \"I don't want to fight! I'm gonna pee myself!\"");
             System.out.println(super.name + " has run away in the direction of the bathroom.");
+            super.relieveOneSelf();
         } else if(super.drunkMeter >= 0.5){
             System.out.println(super.name + " swings at you but misses and faceplants on the ground. Their friend calls them an Uber home.");
         } else if(this.anger >= -.5){
@@ -55,7 +57,6 @@ public class NPC extends Person {
             double noticed = Math.random();
             double win = Math.random();
             double reward = (double) (Math.random() * player.getMoneyAmount());
-            if(noticed > 0.9){} // get kicked out?
             if(win >= 0.3){
                 System.out.println("You beat " + this.name + " in a fight!");
                 System.out.println("They gave you $" + reward + " to leave them alone.");
@@ -65,6 +66,7 @@ public class NPC extends Person {
                 System.out.println("They stole $" + reward + " out of your wallet.");
                 player.addMoney(-1 * reward);
             }
+            if(noticed > 0.9){bouncer.kickOut();}
         }
     }
 
