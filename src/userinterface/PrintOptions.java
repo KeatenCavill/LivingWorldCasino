d7651d692be1239c322e13fc2df6e90e7d389d85
Drew Saltzman <drewsaltzman16@gmail.com>
2025-12-07 18:52:55 -0700

    Some more userinterface logic.

--- src/userinterface/PrintOptions.java
+++ src/userinterface/PrintOptions.java
@@ -1,11 +1,14 @@
 package userinterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PrintOptions {
 
-    static String[] currentEnv(core.Environment env){
+    static List<String> currentEnv(core.Environment env){
         numOptions = env.connnectedAreas.size();
-        String[] options = new String[] {""};
+        List<String> options = new String[] {""};
         return options;
     }
 
--- src/userinterface/UserIO.java
+++ src/userinterface/UserIO.java
@@ -1,16 +1,52 @@
 package userinterface;
+
+import core.Environment;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserIO {
     
-    static void input(Scanner inStream){
-        try {
+
+    static List<String> commands = List.of("status","back","help");
+
+
+    static void input(Scanner inStream,Environment env){
+
+        String input;
+        Boolean validInput = false;
+        List<String> options = PrintOptions.currentEnv(env);
+
+        while (!validInput){
+            try {
+                input = inStream.nextLine();
+
+                if (commands.contains(input)){
+
+                    validInput = true;
+                }
+                else if (options.contains(input)){
+
+                    validInput = true;
+                }
+                else {
+                    validInput = false;
+                }
+            } 
+            catch (InputMismatchException e) {
+                System.out.println("Input must be an command or listed option.");
+                System.out.println("For a list of commands, type help.");
+                validInput = false;
+            }
+        }
+
+        switch (input){
             
-        } 
-        catch (InputMismatchException e) {
 
         }
+
     }
 
 }
